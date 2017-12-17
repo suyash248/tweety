4e6742716512375517443adc68a23d6a56359da0
Suyash Soni <suyash.soni248@gmail.com>
2017-12-17 21:03:55 +0530

    Custom querybuilder

--- app.py
+++ app.py
@@ -9,6 +9,37 @@ def index():
     Tweety().filter(keywords=['cricket'])
     return 'Welcome to Tweety!'
 
+@app.route('/', methods=["POST"])
+def search():
+    pass
+
+@app.route('/test')
+def test():
+    from service.esutil.querybuilder.query_builder import QueryBuilder
+    criteria = {
+        "AND": {
+            "fields": ['tweet_text'],
+            "operator": "contains",
+            "query": "cricket"
+        }
+    }
+
+    s = QueryBuilder(criteria).search()
+    print QueryBuilder.get_raw_query(s)
+    for hit in s:
+        print hit.tweet_text
+    # from service.esutil import dsl_search
+    # from elasticsearch_dsl import Q
+    # q = Q("multi_match", query='cricket', fields=['tweet_text'])
+    # s = dsl_search.query(q)
+    # print s.to_dict()
+    #
+    # print s.execute()
+    # for hit in s:
+    #     print hit.tweet_text
+    #
+    return "Testing"
+
 
 if __name__ == '__main__':
     app.run()
